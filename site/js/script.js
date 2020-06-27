20492a10c800561af5929c35d02dc3f11f8f48e1
Oluwatobi <bamiddy@gmail.com>
2020-06-27 13:46:52 +0100

    My example

---
+++ site/js/script.js
@@ -0,0 +1,17 @@
+$(function () {
+    
+    $("#navbarToggle").blur(
+        function () {
+            var screenWidth = window.innerWidth;
+            if (screenWidth < 768){
+                $("#collapsable-nav").collapse("hide")
+            }
+       
+        }
+    )
+
+        
+   
+
+
+});
